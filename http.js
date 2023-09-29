83c8bf8aec6e25ff6b2c928fdcc6b3ef6627714e
Daniel Won <wonsy910@gmail.com>
2023-09-28 20:57:12 -0400

    uploading finished scripts

---
+++ http.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+const url = ""; //put api url here.
+
+http.request(url, (response) => {
+	let data = "";
+	response.on("data", (chunkData) => {
+		data = data + chunckData.toString(); //bring data from the api and convert it into String, then overlap on empty "data"
+	});
+	response.on("end", () => {
+		const content = JSON.parse(data); //Important data section from the source(api)
+		console.log(content);
+	});
+});
+
+request.on("error", (error) => {
+	console.log("An error", error); //Error scenario
+});
+
+request.end();
